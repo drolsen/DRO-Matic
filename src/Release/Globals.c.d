2f8df577ccb1acf25c902c51b6e9e54101d26acc
dolsen <dolsen@connectivedx.com>
2017-08-20 12:26:12 -0700

    Cleaning up gitignore

--- src/Release/Globals.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\dolsen\AppData\Local\Temp\VMicroBuilds\DROMatic\mega_atmega2560\Globals.c.o: \
- C:\Users\dolsen\AppData\Local\Temp\VMicroBuilds\DROMatic\mega_atmega2560\Globals.c \
- C:\Users\dolsen\AppData\Local\Temp\VMicroBuilds\DROMatic\mega_atmega2560\Globals.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h
